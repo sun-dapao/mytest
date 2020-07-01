1afdb45f8404d82acff479816b8459640442c3f4
yihan.sun <sunyh@jihualab.com>
2020-07-01 17:38:52 +0800

    Initial commit

---
+++ src/components/home/Appindex.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      Hello world
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Appindex'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import AppIndex from '@/components/home/AppIndex'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
+import WorkReportIndex from '../components/workreport/WorkReportIndex'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/workreport',
+          name: 'WorkReport',
+          component: WorkReportIndex,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
+    }
+  ]
+})
